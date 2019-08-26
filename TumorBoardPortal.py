e40025d3179fcde93436aae75266d91c3733f02f
tizatt <tylerizatt@gmail.com>
2019-08-26 15:26:25 -0700

    Reformatted json output

--- TumorBoardPortal.py
+++ TumorBoardPortal.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import pymongo
+import json
 import pprint
 
 
@@ -7,67 +8,98 @@ client = MongoClient("labdb01.tgen.org", 25755)
 annotateDb = client['AnnotateBy']
 geneCollection = annotateDb['gene']
 
+statsDb = client['statsTemp']
+componentsCollection = statsDb['components']
+
 db = client['markers']
 tumorCollection = db['tumor']
 study = "C024"
 origin = "DNA"
 
+def getPatientId( studyPatientTissue ):
+    project_run_search = studyPatientTissue[:-3]
+    patient_result = componentsCollection.find({'projectRun': {'$regex' : project_run_search}},{'kb.patients.studyPatientID':1})
+    id = ""
+    for result in patient_result:
+        result_id = result['kb']['patients']['studyPatientID']
+        print(result_id)
+        if result_id:
+            id = result_id
+
+    return id
+
+
 
 def getCancerCensus():
-    cancerCensusGenes = geneCollection.find({'cancerCensus': {"$exists": "true"}},{"gene":1})
-    ccGenes = []
-    for ccGene in cancerCensusGenes:
-        ccGenes.append(ccGene['gene'])
-    return ccGenes
+    cancer_census_genes = geneCollection.find({'cancerCensus': {"$exists": "true"}},{"gene":1})
+    cc_genes = []
+    for ccGene in cancer_census_genes:
+        cc_genes.append(ccGene['gene'])
+    return cc_genes
 
 
-def printBiomarker( patient, variants ):
-    biomarkers = {"biomarkers" : variants,
+def print_biomarker( patient, variants ):
+    biomarkers = {
+                  "api_version" : "2.0.1",
                   "source" : "TGen",
-                  "drug_rules_version": "1.3",
-                  "order": {"tumor_collection_date":"01/01/2000", "primary_diagnosis": "", "specimen_type" : "", "specimen_site": ""}
-                  }
-    file = open(patient + ".json", "w")
-    pp = pprint.PrettyPrinter(indent=4)
-    pprint.pprint(biomarkers, file)
-    #pprint.pprint(biomarkers, file)
+                  "drug_rules_version": "82",
+                  "biomarkers": variants
+    }
+    biomarkers_json = json.dumps(biomarkers)
+    file = open("patients/" + patient + ".json", "w")
+
+    file.write(biomarkers_json)
+    file.close()
+
+
 def getVariants():
     ccGenes = getCancerCensus()
     studyResult = tumorCollection.find({"variants.study" : study, 'variants.filter' : 'PASS', "gene" : { "$in": ccGenes},
                                     'aberration.aberration_type2': {"$ne": "Synonymous"},
-                                    "$or": [{'aberration.aberration_type': 'SNV'},
+                                    "$or": [{'variants.filetype' : 'seuratPointMutationFile'},
+                                                      {'variants.filetype' : 'freebayesFile'}],
+                                    "$or": [
+                                            {'aberration.aberration_type':'SNV'},
                                             {'aberration.aberration_type': 'FUSED_GENE'},
                                             {'aberration.aberration_type': 'CNV_LOSS'},
-                                            {'aberration.aberration_type': 'CNV_GAIN'},
-                                            {'aberration.aberration_type': 'INDEL'}
+                                            {'aberration.aberration_type': 'CNV_GAIN'}
                                             ]
                                     }
-                                   ).sort("variants.studyPatient")
+                                   ).sort([("variants.studyPatientTissue", pymongo.ASCENDING)])
     return studyResult
 
 def parseVariants(studyResult):
     variants = []
-    patients = []
-    currentPatient = ""
+
+    current_patient = ""
+
     for result in studyResult:
+
         gene = result['gene']
         alteration_type = result['aberration']['aberration_type2']
         origin = "DNA"
-        patient = result['variants'][0]['studyPatient']
-        effect = result['aberration']['aberration_value']
-        tissue = result['variants'][0]['tissue']
+        studyPatientTissue = result['variants'][0]['studyPatientTissue']
+
+        if not current_patient:
+            print( "changing current patient to : "+studyPatientTissue)
+            current_patient = studyPatientTissue
+
+        if 'AminoAcidChange' in result['snpeff']:
+            effect = result['snpeff']['AminoAcidChange']
+        else:
+            effect = result['aberration']['aberration_value']
+
+        if studyPatientTissue != current_patient:
+            # id = getPatientId(studyPatientTissue)
+            print_biomarker(current_patient, variants)
+            variants = []
+            current_patient = studyPatientTissue
+            # print( patient + " " + gene + " " + alteration_type + " " + origin + " " + effect + " " + tissue)
 
         variant = { "gene": gene, "alteration_type": alteration_type, "origin": origin, "effect" : effect}
         variants.append( variant )
 
-        if patient == "":
-            currentPatient = patient
 
-        if patient != currentPatient:
-            currentPatient = patient
-            printBiomarker( patient, variants )
-            variants = []
-            #print( patient + " " + gene + " " + alteration_type + " " + origin + " " + effect + " " + tissue)
 
 
 
