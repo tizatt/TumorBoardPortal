0e3a79e33a55d5d2d6269eecb31416c9ab9dbd3a
Tyler Izatt <tizatt@diva.tgen.org>
2019-09-25 13:29:04 -0700

    Added clinical info method

--- TumorBoardPortal.py
+++ TumorBoardPortal.py
@@ -16,6 +16,9 @@ study = "C024"
 origin = "DNA"
 
 
+# disease = visit.diagnosis
+#
+
 def get_patient_id(study_patient_tissue):
     convert_plan_c024 = []
     with open('PLAN_C024.txt') as f:
@@ -37,7 +40,12 @@ def get_cancer_census():
     return cc_genes
 
 
-def print_biomarker(patient, variants):
+def get_clinical_info( project_run ):
+    
+
+def print_biomarker(patient, project_run, variants):
+    
+    clinical_info = get_clinical_info( project_run )
     biomarkers = {
         "api_version": "2.0.1",
         "source": "TGen",
@@ -78,7 +86,7 @@ def getVariants():
 
 def parseVariants(study_result):
     variants = []
-
+    project_run = ""
     current_patient = ""
     origin = "DNA"
 
@@ -87,7 +95,8 @@ def parseVariants(study_result):
         gene = result['gene']
         alteration_type = result['aberration']['aberration_type2']
         dna_allele_freq = ""
-        study_patient_tissue = result['variants'][0]['studyPatientTissue']
+        project_run = result['variants'[0]['projectRun']
+	study_patient_tissue = result['variants'][0]['studyPatientTissue']
 
         if 'SEURAT_AR_TUMOR' in result['variants'][0]:
             dna_allele_freq = result['variants'][0]['SEURAT_AR_TUMOR']
@@ -119,10 +128,11 @@ def parseVariants(study_result):
             print_biomarker(current_patient, variants)
             variants = []
             current_patient = study_patient_tissue
-
+    
         variant = {"gene": gene, "alteration_type": alteration_type, "origin": origin, "effect": effect, "dna_allele_frequency": dna_allele_freq}
         variants.append(variant)
 
+    print_biomarker("C024_0008_T1", project_run, variants)
 
 queryResult = getVariants()
 parseVariants(queryResult)
